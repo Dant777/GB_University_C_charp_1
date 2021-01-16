d4adeee02917a6ab338117b751f13ed4b08b3285
Dant777 <kolbt87@gmail.com>
2021-01-16 22:40:24 +0300

    Losson #1 has done

---
+++ Lesson_1/Lesson_1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+/*
+ * ЗАДАНИЕ:
+ * Написать программу, выводящую в консоль текст: «Привет, %имя пользователя%, сегодня %дата%».
+ * Имя пользователя сохранить из консоли в промежуточную переменную.
+ * Поставить точку останова и посмотреть значение этой переменной в режиме отладки.
+ * Запустить исполняемый файл через системный терминал.
+ *
+ */
+namespace Lesson_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Введите имя:");
+            string userName = Console.ReadLine();
+
+            Console.WriteLine($"Привет, {userName}, сегодня {DateTime.Now.ToShortDateString()}");
+
+            Console.ReadKey();
+
+        }
+    }
+}
